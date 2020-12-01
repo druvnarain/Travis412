b7dbf3c2d4122056b73c438f23c2810461cf1604
druvnarain <50425550+druvnarain@users.noreply.github.com>
2020-11-30 20:11:31 -0500

    added test methods for minus, multiply, divide

--- src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
+++ src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
@@ -11,4 +11,22 @@ public class SimpleCalculatorTest {
 		SimpleCalculator calc = new SimpleCalculator();
 		assertEquals(calc.add(1, 1), 0);
 	}
+	
+	@Test
+	public void testMinus() {
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.minus(5, 3), 2);
+	}
+	
+	@Test
+	public void testDivide() {
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.divide(4, 2), 2);
+	}
+	
+	@Test
+	public void testMultiply() {
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.multiply(5, 3), 15);
+	}
 }
